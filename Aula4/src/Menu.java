53408ac6d9ed27f50b94d9d006157c23693f09de
Gismi Guimarães <97984496+Gismii@users.noreply.github.com>
2022-07-24 00:17:48 -0300

    Programando em java

--- Aula4/src/Menu.java
+++ Aula4/src/Menu.java
@@ -1,30 +1,35 @@
-import java.awt.*;
+//import java.lang.String;
 import java.util.Scanner;
 
 public class Menu {
- public static void main(String[] args) {
-     Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //int escolhas=0;
+        System.out.println("Escolha uma das opções 1-True, 0-false :");
 
-     String[] menu = {"1","2"};
+        String menu = scanner.next();
 
-     System.out.println("Escolha : ");
-     //System.out.println("1 - Sim,  0 - Não");
+        while (menu =="1" || menu =="0") {
 
-     String n = scanner.next();
-     for(int i =0; i<2; i++) {
-         System.out.println("1 - Sim,  0 - Não");
+            System.out.println(" Escolha uma das opções 1-True, 0-false :");
+            menu = scanner.next();
+            if ( menu !="0" && menu !="1") {
+                System.out.println("Opção invalida!! Escolha uma das opções 1-True, 0-false :");
+                menu = scanner.next();
 
-         if (n != "1" && n != "0") {
-             System.out.println("Encerrado!");
+                if (menu == "-5")
 
-         }
+                    System.out.println("FINALIZADO PELO USUSARIO!!");
 
-
-
-     }
-
+            }
+        }
 
 
-
+    }
 }
-}
+
+
+
+
+
+
